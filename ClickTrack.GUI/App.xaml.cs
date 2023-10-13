376d5cc5dbd14862a25aa09f221d9422e90a8a33
Josh Bullock <mail@joshb.me>
2023-10-13 19:00:03 -0400

    Add GUI project

---
+++ ClickTrack.GUI/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace ClickTrack.GUI;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
---
+++ ClickTrack.Lib/Library.fs
@@ -0,0 +1,5 @@
+﻿namespace ClickTrack.Lib
+
+module Say =
+    let hello name =
+        printfn "Hello %s" name
